5760a09ed6d2994b4392b54bfdcfeb59b849e6f6
Mike Vegeto <mike@bedrockdata.com>
2017-04-30 02:29:45 -0400

    clean up topology defs

--- topology.js
+++ topology.js
@@ -1,20 +1,10 @@
 module.exports = {
   name: 'attak-example',
   processors: {
-    hello_world_spout: {
-      source: './processors/hello_world_spout'
-    },
-    reverse: {
-      source: './processors/reverse'
-    }
+    hello_world_spout: './processors/hello_world_spout',
+    reverse: './processors/reverse'
   },
   streams: [
-    {
-      to: 'reverse',
-      from: 'hello_world_spout',
-      fields: {
-        source_field_1: 'processor_field_1'
-      }
-    }
+    ['reverse', 'hello_world_spout']
   ]
 };
